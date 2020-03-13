b9fc8cc6d46886ae563ee65cc3447172afb91516
Ahmed Elgaidi <49118737+AhmedElgaidi@users.noreply.github.com>
2020-03-13 02:45:23 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// our refrences
+const clock = document.querySelector('.clock');
+
+const tick = () => {
+    const now = new Date();
+    const hours = now.getHours(),
+            mins = now.getMinutes(),
+            secs = now.getSeconds(),
+             ampm = hours >= 12 ? 'pm' : 'am';
+    const html = `
+        <span>${hours}</span> :
+        <span>${mins}</span> :
+        <span>${secs}</span> :
+        <span>${ampm.toUpperCase()}</span> 
+        `
+    clock.innerHTML = html;
+};
+setInterval(tick, 1000);
+
